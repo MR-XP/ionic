1af717810c661a43368adbe16425813e9fd0c81c
xp <xp13638232495@outlook.com>
2018-03-29 00:35:25 +0800

    更新中

--- js/app.js
+++ js/app.js
@@ -1,29 +1,27 @@
 window.HX_CONFIG = {
     loadingBase: {template: '<ion-spinner icon="android" class="spinner spinner-android" style="stroke: #fff"><svg viewBox="0 0 64 64"><g transform="rotate(315.7000000000001,32,32)"><circle stroke-width="6" stroke-dasharray="130.10559854346837" stroke-dashoffset="0.33136094674554784" r="26" cx="32" cy="32" fill="none" transform="scale(1,1) translate(0,0) rotate(-270,32,32)"></circle></g></svg></ion-spinner>'},
-    HOST: '',
-    TEMPLATE_HOST: "",
-    STATIC_HOST: '',
+    HOST			: 		"",
+    TEMPLATE_HOST	: 		"",
+    STATIC_HOST		: 		"",
+    VERSION	   		: 		"",
+    PAGEVERSION		:		"?v=1.0"
 };
 
 // Ionic Starter App
 angular.module('App', ["ionic", "ngAnimate", "App.ctrls", "oc.lazyLoad", "App.filters", "App.directives","App.services"])
 
-    .run(["$ionicPlatform", "$ionicLoading", "$rootScope", "$state", "$location","$ionicPopup",
-        function ($ionicPlatform, $ionicLoading, $rootScope, $state, $location,$ionicPopup) {
+    .run(["$ionicPlatform", "$ionicLoading", "$rootScope", "$state", "$location",
+        function ($ionicPlatform, $ionicLoading, $rootScope, $state, $location) {
 
             $ionicLoading.show(HX_CONFIG.loadingBase);
 
 /*			
 			//验证登录
-            var missLoginView = ["login", "find-pwd"];		//不需要登录的页面
+            var missLoginView = ["login", "reg"];		//不需要登录的页面
            
             $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams, options) {
                
             	if (missLoginView.indexOf(toState.name) == -1 && !window.member.id){
- 
-                    $ionicPopup.alert({
-                    	title		:	'请登录',
-                    });
                     
                     $state.go('/login');									//跳转到登录页
 					
@@ -34,22 +32,38 @@ angular.module('App', ["ionic", "ngAnimate", "App.ctrls", "oc.lazyLoad", "App.fi
             });
 */
 
+			//you know
             $rootScope.back = function () {
                 history.back();
             };
             
+            //静态跳转
             $rootScope.location = function (path, type) {
-                $ionicLoading.show(HX_CONFIG.loadingBase);
-                if (type) {
-                    path = $state.href(path).replace("#", "");
-                }
+
+                if (type) 
+                    path = $state.href(path).replace("#", "")
+                
                 $location.path(path);
             };
             
+            //阻止冒泡
             $rootScope.stopBubbling = function (e) {
                 e.stopPropagation();
             };
             
+            //title
+            $rootScope.setDocumentTitle = function(title) {
+		    	document.title = title;
+			   	i = document.createElement('iframe');
+		        i.src = '/favicon.ico';
+		        i.style.display = 'none';
+		        i.onload = function() {
+		            setTimeout(function(){
+		                i.remove();
+		            }, 9)
+		        }
+		        document.body.appendChild(i);
+			};
             
         }])
 		
@@ -65,17 +79,20 @@ angular.module('App', ["ionic", "ngAnimate", "App.ctrls", "oc.lazyLoad", "App.fi
 		
 		$urlRouterProvider.when('/users', '/users/index');			     
 		
-		$urlRouterProvider.otherwise('/users/index');
+		//判断是否跳转支付,没有默认users模块
+		var nowUrl=location.href.substr(location.href.lastIndexOf('/')+1,8);
+		if(nowUrl!='pay.html')
+			$urlRouterProvider.otherwise('/users')
 		
 
 		//控制器加载
         $stateProvider
 		
 		//单个加载
-		.state('login', {
-            url: '/login',
-            templateUrl: HX_CONFIG.TEMPLATE_HOST + "tpl/login.html",
-            controller: 'loginCtrl',
+		.state('reg', {
+            url: '/reg',
+            templateUrl: HX_CONFIG.TEMPLATE_HOST + "tpl/reg.html"+HX_CONFIG.PAGEVERSION,
+            controller: 'regCtrl',
             cache: false
         })
 
@@ -86,7 +103,7 @@ angular.module('App', ["ionic", "ngAnimate", "App.ctrls", "oc.lazyLoad", "App.fi
 			template: '<ion-nav-view class="slide-left-right"></ion-nav-view>',
 			resolve: {
 				ollc: ['$ocLazyLoad', function ($ocLazyLoad) {
-					return $ocLazyLoad.load(HX_CONFIG.STATIC_HOST + 'js/usersCtrls.js').then(function () {
+					return $ocLazyLoad.load(HX_CONFIG.STATIC_HOST + 'js/usersCtrls.js'+HX_CONFIG.PAGEVERSION).then(function () {
 						console.log("js load ok");
 					});
 				}]
@@ -96,18 +113,17 @@ angular.module('App', ["ionic", "ngAnimate", "App.ctrls", "oc.lazyLoad", "App.fi
 			//模块子类
 			.state('users.index', {
 				url: '/index',
-				templateUrl: HX_CONFIG.TEMPLATE_HOST + "tpl/users/index.html",
+				templateUrl: HX_CONFIG.TEMPLATE_HOST + "tpl/users/index.html"+HX_CONFIG.PAGEVERSION,
 				cache: false
 			})
-		
-		
+
 			.state('users.list', {
 				url: '/list',
 				templateUrl: HX_CONFIG.TEMPLATE_HOST + "tpl/users/list.html",
 				resolve:{
 					ollc:['$ocLazyLoad',function($ocLazyLoad){
-						return 	$ocLazyLoad.load(HX_CONFIG.STATIC_HOST + 'js/plugins/jquery.GM.js'),
-								$ocLazyLoad.load(HX_CONFIG.STATIC_HOST + 'css/app.css');
+						return 	$ocLazyLoad.load(HX_CONFIG.STATIC_HOST + 'js/plugins/jquery.GM.js'+HX_CONFIG.PAGEVERSION),
+								$ocLazyLoad.load(HX_CONFIG.STATIC_HOST + 'css/app.css'+HX_CONFIG.PAGEVERSION);
 					}]
 				},
 				cache: false
--- js/ctrls.js
+++ js/ctrls.js
@@ -1,5 +1,5 @@
 var app = angular.module('App.ctrls', []);
-app.controller("indexCtrl", ["$scope","$timeout","$interval","$state","$stateParams","$rootScope","$location","$filter","$compile","$ionicLoading","$ionicPopup","$ionicModal","ajaxService",
+app.controller("showCtrl", ["$scope","$timeout","$interval","$state","$stateParams","$rootScope","$location","$filter","$compile","$ionicLoading","$ionicPopup","$ionicModal","ajaxService",
 				   function ($scope,
 							$timeout,
 							$interval, 
@@ -16,7 +16,7 @@ app.controller("indexCtrl", ["$scope","$timeout","$interval","$state","$statePar
 							
 							ajaxService) {
 
-	$scope.title="首页";
+	$scope.title="展示";
 	
 	
 	//页面刚加载			   
@@ -36,55 +36,148 @@ app.controller("indexCtrl", ["$scope","$timeout","$interval","$state","$statePar
 	
 }]);
 
-app.controller("listCtrl", ["$scope","$ionicLoading","ajaxService","$ionicScrollDelegate",
-				   function ($scope,$ionicLoading,ajaxService,$ionicScrollDelegate) {
-
-	
-	var page = function(){
-    	return {
-            currPage  : 1,
-            totalPage : 2,
-            list      : []
-        }
-    }
-	$scope.page = page();
-	
-	$scope.load=function(cb){
+app.controller("regCtrl", ["$scope", "$ionicLoading", "ajaxService", "$timeout", "$ionicPopup","$location","$popup",
+				  function($scope, $ionicLoading, ajaxService, $timeout, $ionicPopup,$location,$popup) {
 		
-		if(cb)
-			cb()
+		//you know
+		$scope.user = {
+
+			name: '',
+			phone: '',
+			code: ''
+
+		}
+
+		//点击短信验证码
+		var countdown = 60;
+		$scope.ck = function() {
+
+			if($scope.user.phone && $scope.user.phone.length == 11) {
+
+				$scope.settime()
+
+			} else {
+
+				$popup.alert('尊敬的用户', null, '请正确填写手机号')
+
+			}
+
+		}
+
+		//倒计时
+		$scope.settime = function() {
+
+			//倒计时
+			if(countdown == 0) {
+				$('.ipt-code').prop("disabled", false);
+				$('.ipt-code').val('发送手机验证码');
+				countdown = 60;
+				return;
+			} else {
+
+				//请求短信
+				if(countdown == 60)
+					$scope.getCode()
+
+				$('.ipt-code').prop("disabled", true);
+				$('.ipt-code').val('重新发送(' + countdown + ')')
+				countdown--;
+
+			}
+
+			$timeout(function() {
+				$scope.settime()
+			}, 1000)
+
+		}
+
+		//获取验证码
+		$scope.getCode = function() {
+
+			ajaxService.ajax('getCode', {
+				
+				phone	:	$scope.user.phone
+			
+			}, function(json) {
+
+				if(json.code == 200) {
+
+					//$popup.alert('尊敬的用户', null, '发送成功');
+
+				} else {
+
+					$popup.alert('尊敬的用户', null, json.message);
+
+					countdown = 0;
+				}
+
+			})
+
+		}
+
+		//提交
+		$scope.save = function() {
+			
+			ajaxService.ajax('sign', {
+
+				real_name			:		$scope.user.name,
+				phone				:		$scope.user.phone,
+				code				:		$scope.user.code
+
+			}, function(json) {
+
+				if(json.code == 200) {
+
+					var a=$ionicPopup.alert({
+			       	  title    : '尊敬的用户', 
+			       	  subTitle : null,
+			       	  template : '注册成功！即将跳转...',
+			       	  cssClass : 'tc',
+			       	  okText   : '确定', 
+			       	  okType   : '',
+	        		});
+	
+					$timeout(function(){
+						a.close();
+						window.location.reload()								
+					},500)
+
+				} else {
+					$popup.alert('尊敬的用户', null, json.message);
+				}
+
+			})
+
+		}
+		
+		//替换多余非汉字
+		$scope.relEn=function(txt){
+			
+			$scope.user.name=txt.replace(/[^\u4E00-\u9FA5]/g,'');
+			
+		}
+		
+		//替换多余非数字
+		$scope.relCn=function(txt){
+			
+			$scope.user.code=txt.replace(/\D/gi,'');
+			
+		}
+		
+		//页面刚加载			   
+		$scope.$on('$ionicView.beforeEnter', function() {
+			$ionicLoading.hide()			
+		});
+
+		//页面加载完成
+		$scope.$on('$ionicView.afterEnter', function() {
+
+		});
+
+		//页面离开之前
+		$scope.$on('$ionicView.beforeLeave', function() {
+
+		});
+
 	}
-	
-	//下拉刷新
-    $scope.doPTRefresh = function () {
-        $scope.page.currPage = 1;
-        $scope.load(function(){
-        	 $scope.$broadcast('scroll.refreshComplete');
-        });
-
-    };
-    
-    //无限加载回调
-    $scope.onPTLoadMore = function () {
-        $scope.load(function(){
-        	$scope.$broadcast('scroll.infiniteScrollComplete');
-        });
-    };
-
-	
-	//页面刚加载			   
-	$scope.$on('$ionicView.beforeEnter',function(){
-		
-	});
-	
-	//页面加载完成
-	$scope.$on('$ionicView.afterEnter',function(){
-		$ionicScrollDelegate.$getByHandle('mainScroll').scrollTop(false);
-	});
-	
-	//页面离开之前
-	$scope.$on('$ionicView.beforeLeave',function(){
-		
-	});
-	
-}]);
+]);
